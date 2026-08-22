b18b4292d8f727b6bb2d48a42d9ad43604096a4c
Oljira Likasa <oljelikonafo@gmail.com>
2026-08-22 09:06:18 -0300

    Fix Vercel frontend deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -15,11 +15,20 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;*/
-
+/*
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
 };
+
+export default nextConfig;*/
+
+
+
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
